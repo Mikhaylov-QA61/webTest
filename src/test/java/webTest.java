68dca46e62a919b40ec2ea418bb0d5a4ea32ce58
Pers test <qa61test@gmail.com>
2023-07-18 21:23:15 +0400

    Selenium and Selenide test

--- src/test/java/webTest.java
+++ src/test/java/webTest.java
@@ -1,41 +1,62 @@
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class webTest {
 
       // Информация ниже для запуска webDriver для работы на Selenium
-//    WebDriver driver;
-//
-//    @BeforeAll
-//    static void setupAll() {
-//        WebDriverManager.chromedriver().setup();
-//    }
-//
-//    @BeforeEach
-//    void setup() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    void teardown() {
-//        driver.quit();
-//    }
-//
-//    @Test
-//    void test() {
-//        // Здесь должны быть тесты на Selenium
-//    }
+    WebDriver driver;
 
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    // Автотест на Selenium
     @Test
-    void CTest() {
+    void test() {
+        driver.get("http://localhost:9999");
+        WebElement form = driver.findElement(By.className("form"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Иван");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79176666666");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
+
+    //Автотест на Selenide
+    @Test
+    void SelenideApplicationSentTest() {
         open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Петров Иван");
+        form.$("[data-test-id=phone] input").setValue("+79176666666");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
